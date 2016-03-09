6df2a2876672036528beeba3d203cb191946608f
Loquacious <Loquacious@users.noreply.github.com>
2016-03-09 14:25:49 -0500

    Imported with updates from /Loquacious/haiti/

--- scripts/main.js
+++ scripts/main.js
@@ -4,65 +4,64 @@ $.getScreenWidth = function () {
 $(function () {
 	FastClick.attach(document.body);
 	var showabout = true;
-	$('#showabout').removeClass('showabout-active').click(function () {
-		$('#about').show();
-		showabout = !($('#about').css('display') === 'none');
-		console.log('$(\'#showabout\')');
+	$('.showabout').removeClass('showabout-active').click(function () {
+		$('.about').show();
+		showabout = $('.about').css('display') !== 'none';
 		if (showabout) {
-			$('#showabout').css('background-image', 'none');
+			$('.showabout').css('background-image', 'none');
 			if ($.getScreenWidth() > 40) {
-				$('#share').css('margin-left', '25rem');
+				$('.share').css('margin-left', '25rem');
 			}
 		}
 	});
 	if ($.getScreenWidth() <= 80) {
-		$('#about img').show();
+		$('.about img').show();
 	}
-	$('#about img').click(function () {
-		console.log('$(\'#about img\')');
+	$('.about img').click(function () {
 		if ($.getScreenWidth() <= 80) {
-			$('#showabout').css('background-image', 'url(images/v2.svg)').show();
-			$('#about').hide();
-			$('#share').css('margin-left', '0');
-			$('#showabout').addClass('showabout-hover');
+			$('.showabout').css('background-image', 'url(images/v2.svg)').show();
+			$('.about').hide();
+			$('.share').css('margin-left', '0');
+			$('.showabout').addClass('showabout-hover');
 			if ($.getScreenWidth() <= 40) {
-				$('#showabout').css('background-image', 'url(images/v1.svg)');
+				$('.showabout').css('background-image', 'url(images/v1.svg)');
 			}
 		}
 	});
 	$('html').click(function() {
-		console.log('$(\'html\')');
 		if ($.getScreenWidth() <= 80) {
-			$('#showabout').css('background-image', 'url(images/v2.svg)').show();
-			$('#about').hide();
-			$('#share').css('margin-left', '0');
-			$('#showabout').addClass('showabout-hover');
+			$('.showabout').css('background-image', 'url(images/v2.svg)').show();
+			$('.about').hide();
+			$('.share').css('margin-left', '0');
+			$('.showabout').addClass('showabout-hover');
 			if ($.getScreenWidth() <= 40) {
-				$('#showabout').css('background-image', 'url(images/v1.svg)');
+				$('.showabout').css('background-image', 'url(images/v1.svg)');
 			}
 		}
 	});
-	$('#showabout, #about').click(function(event){
-		console.log('$(\'#showabout, #about\')');
+	$('.main img').css('cursor', 'zoom-in').attr('title', 'Click to zoom').click(function () {
+		window.open($(this).attr('src'), '_blank');
+	});
+	$('.showabout, .about').click(function(event){
 		event.stopPropagation();
 	});
 });
 $(window).resize(function() {
 	// @media screen and (max-width: 80rem)
 	if ($.getScreenWidth() <= 80) {
-		$('#about').hide();
-		$('#about img').show()
-		$('#share').css('margin-left', '0');
-		$('#showabout').css('background-image', 'url(images/v2.svg)');
+		$('.about').hide();
+		$('.about img').show()
+		$('.share').css('margin-left', '0');
+		$('.showabout').css('background-image', 'url(images/v2.svg)');
 		// @media screen and (max-width: 40rem)
 		if ($.getScreenWidth() <= 40) {
-			$('#showabout').css('background-image', 'url(images/v1.svg)');
+			$('.showabout').css('background-image', 'url(images/v1.svg)');
 		}
 	// Global styles
 	} else {
-		$('#about').show();
-		$('#about img').hide();
-		$('#share').css({
+		$('.about').show();
+		$('.about img').hide();
+		$('.share').css({
 			'margin-left': '25rem',
 			'background-image': 'none'
 		});
